55649a18a3c7c6691f941541636344076b66f543
Kwen <maokwen@live.com>
2018-09-30 14:39:31 +0800

    实现 plan_execute_update()

--- src_experiment/exp_07_physical_operate/exp_07_05_update.c
+++ src_experiment/exp_07_physical_operate/exp_07_05_update.c
@@ -1,5 +1,6 @@
 //
 // Created by sam on 2018/9/18.
+// Edited bu mao on 2018/9/30.
 //
 #include "physicalplan/physicalplan.h"
 
@@ -13,4 +14,34 @@ int plan_execute_update(dongmendb *db, sql_stmt_update *sqlStmtUpdate , transact
 
     fprintf(stderr, "TODO: update is not implemented yet. in plan_execute_update \n");
 
+    char *tableName = sqlStmtUpdate->tableName;
+    arraylist *fields = sqlStmtUpdate->fields;
+    arraylist *values = sqlStmtUpdate->fieldsExpr;
+
+    physical_scan *scan = physical_scan_generate(db, sqlStmtUpdate->where, tx);
+    scan->beforeFirst(scan);
+
+    while (scan->next(scan)) {
+        for (size_t i = 0; i < fields->size; ++i) {
+            char *currentFieldName = arraylist_get(fields, i);
+            variant *var = (variant *)calloc(sizeof(variant),1);
+            physical_scan_evaluate_expression(arraylist_get(values, i), scan, var);
+
+            if (var->type == DATA_TYPE_INT) {
+                scan->setInt(scan, tableName, currentFieldName, var->intValue);
+            } else if (var->type == DATA_TYPE_CHAR) {
+                /*字符串超出定义时的长度，则截断字符串.*/
+                int max_length_str = scan->getField(scan, tableName, currentFieldName)->length;
+                if (max_length_str < strlen(var->strValue)) {
+                    var->strValue[max_length_str] = '\0';
+                }
+                scan->setString(scan, tableName, currentFieldName, var->strValue);
+            } else {
+                // other data type...
+            }
+        }
+    }
+    scan->close(scan);
+
+    return DONGMENDB_OK;
 };
