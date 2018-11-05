adc5817ff8be6961755e4ce6a202216074fc0713
maokwen <maokwen@live.com>
2018-11-05 23:40:15 +0800

    Update src_experiment/exp_05_algebra_opt/exp_05_02_condition_push_down.c

--- src_experiment/exp_05_algebra_opt/exp_05_02_condition_push_down.c
+++ src_experiment/exp_05_algebra_opt/exp_05_02_condition_push_down.c
@@ -9,34 +9,44 @@
  *
  */
 
+Expression *findTail(Expression *begin, Expression *end) {
+    Expression *iter = begin;
+    while (iter->nextexpr != end && iter->nextexpr != NULL)
+        iter = iter->nextexpr;
+    return iter;
+}
+
+/**
+ * partitionCNF 递归地遍历合取范式的左右分支，拆分条件并把 *sra 包裹成 SRA_SELECT
+ * @param expr 表达式
+ * @param sra 输出参数，sra 的地址
+ * @return expression 的下一个节点
+ *
+ * Select((((a = 1) AND (b = 2)) AND (c = 3)),
+ *      Table(F))
+ * ==>
+ * Select((c = 3),
+ *    Select((a = 1),
+ *       Select((b = 2),
+ *          Table(F))))
+ */
+Expression *partitionCNF(Expression *expr, SRA_t **sra);
+
 
 /*输入一个关系代数表达式，输出优化后的关系代数表达式
  * 要求：在查询条件符合合取范式的前提下，根据等价变换规则将查询条件移动至合适的位置。
  * */
+SRA_t *dongmengdb_algebra_optimize_condition_pushdown(SRA_t *sra) {
 
-/*
-dongmendb>.optimizer select a from F where a = 1 and b = 2 and c = 3;
-.optimizer select a from F where a = 1 and b = 2 and c = 3;
+    SRA_t *ori_select = sra->project.sra;
+    SRA_t *select = ori_select->select.sra;
+    Expression *conds = ori_select->select.cond; //  [((... ... and) ... and) ... and]
 
-Project([a],
-        Select((((a = 1) AND (b = 2)) AND (c = 3)),
-                Table(F)
-        )
-)
+    partitionCNF(conds, &select);
+    return SRAProject(select, sra->project.expr_list);
+}
 
-optimized relational algebra:
-Project([a],
-        Select((c = 3),
-                Select((b = 2),
-                        Select((a = 1),
-                                Table(F)
-                        )
-                )
-        )
-)
-*/
-
-Expression *optimize_condition_pushdown(SRA_t **sra, Expression *expr) {
+Expression *partitionCNF(Expression *expr, SRA_t **sra) {
     if (!expr) return NULL;
     if (expr->term) {
         return expr->nextexpr;
@@ -45,44 +55,27 @@ Expression *optimize_condition_pushdown(SRA_t **sra, Expression *expr) {
         int op_num = operators[expr->opType].numbers;
         expr = expr->nextexpr;
         while (op_num--) {
-            expr = optimize_condition_pushdown(sra, expr);
+            expr = partitionCNF(expr, sra);
         }
         return expr;
     }
 
     if (expr->opType == TOKEN_AND) {
         Expression *left = expr->nextexpr;
-        Expression *right = optimize_condition_pushdown(sra, left);
-        Expression *right_back = optimize_condition_pushdown(sra, right);
+        Expression *right = partitionCNF(left, sra);
+        Expression *right_end = partitionCNF(right, sra);
 
-        Expression *right_tail = right;
-        while (right_tail->nextexpr != right_back && right_tail->nextexpr != NULL)
-            right_tail = right_tail->nextexpr;
+        Expression *right_tail = findTail(right, right_end);
         right_tail->nextexpr = NULL;
+        *sra = SRASelect(*sra, right);
 
-        if (left->opType == TOKEN_AND) {
-            *sra = SRASelect(*sra, right);
-            return right_back;
-        }
+        if (left->opType == TOKEN_AND) // skip left if it already been finished
+            return right_end;
 
-        Expression *left_tail = left;
-        while (left_tail->nextexpr != right)
-            left_tail = left_tail->nextexpr;
+        Expression *left_tail = findTail(left, right);
         left_tail->nextexpr = NULL;
+        *sra = SRASelect(*sra, left);
 
-        *sra = SRASelect(SRASelect(*sra, left), right);
-        return right_back;
+        return right_end;
     }
-}
-
-SRA_t *dongmengdb_algebra_optimize_condition_pushdown(SRA_t *sra){
-
-    SRA_t *ori_select = sra->project.sra;
-    SRA_t *select = ori_select->select.sra;
-    Expression *conds = ori_select->select.cond; //  [((... ... and) ... and) ... and]
-
-    optimize_condition_pushdown(&select, conds);
-    return SRAProject(select, sra->project.expr_list);
-
-    return sra;
 }
